f622ad8780ee8131d5a03bb0415d59d5e751d91c
Williams YEO <wyeo@student.42.fr>
2017-02-07 11:50:59 +0100

    AI implementation

--- src/calculateWinner.js
+++ src/calculateWinner.js
@@ -18,4 +18,88 @@ let calculateWinner = (squares) => {
   return null
 }
 
+const minScore = (squares) => {
+  if (calculateWinner(squares) === 'X') {
+    return 10
+  }
+  else if (calculateWinner(squares) === 'O') {
+    return -10
+  }
+  else {
+    let bestMoveValue = 100
+    for (var i = 0; i < squares.length; i++) {
+      let tmpSquare = validMove(i, 'O', squares)
+      if (tmpSquare) {
+        let predictedMoveValue = maxScore(tmpSquare)
+        if (predictedMoveValue < bestMoveValue) {
+          bestMoveValue = predictedMoveValue
+        }
+      }
+    }
+    return bestMoveValue
+  }
+}
+
+const maxScore = (squares) => {
+  if (calculateWinner(squares) === 'X') {
+    return 10
+  }
+  else if (calculateWinner(squares) === 'O') {
+    return -10
+  }
+  else {
+    let bestMoveValue = -100
+    for (var i = 0; i < squares.length; i++) {
+      let tmpSquare = validMove(i, 'O', squares)
+      if (tmpSquare) {
+        let predictedMoveValue = minScore(tmpSquare)
+        if (predictedMoveValue > bestMoveValue) {
+          bestMoveValue = predictedMoveValue
+        }
+      }
+    }
+    return bestMoveValue
+  }
+}
+
+const validMove = (index, player, squares) => {
+
+  let tmpSquares = squares ? squares.slice(0) : []
+
+  if (!tmpSquares[index]) {
+    tmpSquares[index] = player
+    return tmpSquares
+  } else {
+    return null
+  }
+}
+
+const minimax = (squares) => {
+  let
+    bestMoveScore = 100,
+    move = null
+
+  if (calculateWinner(squares)) {
+    return null
+  }
+
+  for (let i = 0; i < squares.length; i++) {
+    let tmpSquare = validMove(i, 'O', squares)
+    if (tmpSquare) {
+      let moveScore = maxScore(tmpSquare)
+      if (moveScore < bestMoveScore) {
+        bestMoveScore = moveScore
+        move = i
+      }
+    }
+  }
+  return move
+}
+
+// console.log(minimax([
+//   'O', null, 'X',
+//   'X', null, null,
+//   'X', 'O', 'O'
+// ]))
+
 module.exports = calculateWinner
